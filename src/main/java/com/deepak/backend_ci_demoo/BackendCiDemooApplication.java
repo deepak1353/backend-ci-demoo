a5f4c6cf28eef97bc8b9dee007485a73c53fcd4c
Deepak Yadav <Deepakyadav20c@gmail.com>
2026-05-27 01:54:44 +0530

    added ci

--- src/main/java/com/deepak/backend_ci_demoo/BackendCiDemooApplication.java
+++ src/main/java/com/deepak/backend_ci_demoo/BackendCiDemooApplication.java
@@ -10,4 +10,8 @@ public class BackendCiDemooApplication {
 		SpringApplication.run(BackendCiDemooApplication.class, args);
 	}
 
+
+    //there are some topics like workflow ka folder in ci.yaml ,
+    // build acitons konse hote hai, docker compose, golder images / whtelisted/ github ke
+    // self hosted runners/ required checks , like this so make me learn everything sequentially//
 }

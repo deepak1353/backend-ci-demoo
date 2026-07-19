5079f7bec24516375caca6f89ea111d5fa60bbff
Deepak Yadav <Deepakyadav20c@gmail.com>
2026-07-19 14:35:36 +0530

    MyFirstJOb

--- src/main/java/com/deepak/backend_ci_demoo/BackendCiDemooApplication.java
+++ src/main/java/com/deepak/backend_ci_demoo/BackendCiDemooApplication.java
@@ -12,6 +12,26 @@ public class BackendCiDemooApplication {
 
 
     //there are some topics like workflow ka folder in ci.yaml ,
-    // build acitons konse hote hai, docker compose, golder images / whtelisted/ github ke
+    // build acitons konse hote hai, docker compose, rgolder images / whtelisted/ github ke
     // self hosted runners/ required checks , like this so make me learn everything sequentially//
+
+    //THEN We’ll Deep Dive Into
+    //GitHub Actions Internals
+    //workflow syntax
+    //reusable workflows
+    //composite actions
+    //caching
+    //artifacts
+    //secrets
+    //environments
+    //approvals
+    //matrices
+    //THEN Enterprise Topics
+    //self-hosted runners
+    //golden images
+    //whitelisting
+    //required checks
+    //protected branches
+    //flaky handling
+    //parallel runners
 }
